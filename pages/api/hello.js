57fb7370f9801a9d2b55acd96f3a983c45f9111a
Fernando Dingler <fdingler@amazon.com>
2022-11-17 09:48:42 -0800

    Timeout

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  return new Promise((accept) => {
+    setTimeout(() => {
+      res.send("Ok");
+      accept();
+    }, 32000);
+  });
 }
